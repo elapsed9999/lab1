669526736be15ac78d1b482fc2e2c7f0bba5c76f
King-of-Roses <86077882+King-of-Roses@users.noreply.github.com>
2023-11-29 22:06:54 +0100

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -1,16 +1,1 @@
-import java.awt.*;
-
-public abstract class Truck extends Car{
-
-    
-    
-
-    public double speedFactor(){
-        if(rampdown())
-             return 0;
-         return 1;
-
-
-
-}
 
